057f8949db168f67605207ef279b10bf55bfc56c
Jovan <jlee251@u.rochester.edu>
2019-05-25 19:24:58 -0400

    Testing Recommendations

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-
-
+/* Init */
 // Get the hash of the url
-
-
-
 const hash = window.location.hash
 .substring(1)
 .split('&')
@@ -18,7 +14,6 @@ window.location.hash = '';
 
 // Set token
 let _token = hash.access_token;
-
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
@@ -37,58 +32,50 @@ const scopes = [
 'user-read-birthdate',
 'user-read-private'
 ];
-// Users Recently Played Songs
-var usrTracks = [];
-// USers Top Artists
-var usrArtists = [];
 
-var base = 'https://api.spotify.com/v1/recommendations?';
-	var market = 'market=US';
-	var seed_artists= '&seed_artists=4NHQUGzhtTLFvgF5SZesLK';
-	var seed_tracks = '&seed_tracks=0c6xIDDpzE81m2q797ordA&';
-	var min_energy= '&min_energy=0.4';
-	var reccURL = base+market+seed_artists+seed_tracks+min_energy;
-// Get Suggestions from Users Tracks
-	console.log(" BEFORE Getting custom with parameters " + reccURL);
+
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 }
 
-// Make a call using the token
+// Users Recently Played Songs
+var usrTracks = [];
+// USers Top Artists
+var usrArtists = [];
 
+// Reccomendation URL for API Call
+var base = 'https://api.spotify.com/v1/recommendations?';
+var market = 'market=US';
+var seed_artists= '&seed_artists=4NHQUGzhtTLFvgF5SZesLK';
+var seed_tracks = '&seed_tracks=0c6xIDDpzE81m2q797ordA&';
+var min_energy= '&min_energy=0.4';
+var reccURL = base+market+seed_artists+seed_tracks+min_energy;
+	
+
+/* API CALLS */
 // First Step: 
 // API Call Get Users Recently Played Songs (Graph It)
-console.log("Recent Tracks");
 $.ajax({
    url: "https://api.spotify.com/v1/me/player/recently-played",
    type: "GET",
    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
    success: function(data) { 
-  // console.log("Recently played Tracks");
-  // console.log("Recently played Tracks");
-     // Do something with the returned data
 	 usrTracks.push(data);
-    // console.log(data);
    }
 });
 
 
 
-console.log("Top Artists");
-// Second Step: Give Recommendations 
+//Second Step: 
 // Get Users Top Artists
 $.ajax({
    url: "https://api.spotify.com/v1/me/top/artists",
    type: "GET",
    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
    success: function(data) { 
-     // Do something with the returned data
      data.items.map(function(artist) {
-		 //console.log(artist);
 		 usrArtists.push(artist);
-       //let item = $('<li>' + artist.name + '</li>');
-      // item.appendTo($('#top-artists'));
      });
    }
 });
@@ -96,26 +83,10 @@ $.ajax({
 
 	
 
-
+// Third Step:
+// Custom Playlist Recommendation
 function getCustomPL(){
 	console.log("AFTER Getting custom with parameters " + reccURL);
-	/*$.ajax({
-   url: reccURL,
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-   success: function(data) { 
-   for(var i=0;i<data.items.length;i++){
-	   console.log("Attempting "  + reccURL);
-   let suggTrack = $('<li>' + data.items[i].track.name + " " + data.items[i].track.artist[0].name +  + '</li>');
-          suggTrack.appendTo($('#top-artists'));
-
-   }
-  // console.log(data);
-     // Do something with the returned data
-   
-   
-}); */
-console.log("update");
 $.ajax({
    url: reccURL, 
    type: "GET",
@@ -127,19 +98,10 @@ $.ajax({
        item.appendTo($('#top-artists'));
    }
    
-     // Do something with the returned data
-     /*data.items.map(function(artist) {
-		 //console.log(artist);
-		 usrArtists.push(artist);
-       let item = $('<li>' + artist.name + '</li>');
-       item.appendTo($('#top-artists'));
-     }); */
    }
 });
 }
-// API Call Directory
 
-// Users Recetly Played Songs
 
 
 console.log("update");
